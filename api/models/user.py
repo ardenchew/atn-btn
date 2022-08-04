d29fc1c8a481d74fa855c3c13cb1de1350be281b
Arden Chew <achew@grailbio.com>
2022-08-04 01:57:35 -0700

    working user creation mwow

---
+++ api/models/user.py
@@ -0,0 +1,11 @@
+from app import db
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, nullable=True)
+
+    def __init__(self, email=None, **kwargs):
+        db.Model.__init__(self, email=email, **kwargs)
